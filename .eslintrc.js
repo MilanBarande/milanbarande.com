ba0aa5f44eb681b6016541c62054cc3f908a4103
MilanBarande <milan.barande@gmail.com>
2022-03-24 13:44:04 +0100

    setup eslint, prettier and tailwind html template

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  plugins: ['simple-import-sort'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:security/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  rules: {
+    'no-console': 'warning',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'unicorn/filename-case': 'off'
+  }
+};
