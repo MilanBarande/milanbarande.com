09d229f9267c004e809949cf231ff1bbdd928276
MilanBarande <milan.barande@gmail.com>
2022-03-24 13:54:59 +0100

    add some info

--- pages/index.js
+++ pages/index.js
@@ -62,24 +62,24 @@ export default function Home() {
                 <div className="flex justify-between">
                   <div className="text-gray-400">Location</div>
                   <div className="font-medium text-right text-gray-600">
-                    London
+                    Paris, France
                   </div>
                 </div>
                 <div className="flex justify-between">
                   <div className="text-gray-400">Experience</div>
                   <div className="font-medium text-right text-gray-600">
-                    3+ years
+                    4+ years
                   </div>
                 </div>
                 <div className="flex justify-between">
                   <div className="text-gray-400">Availability</div>
                   <div className="font-medium text-right text-gray-600">
-                    1 week
+                    1 month
                   </div>
                 </div>
                 <div className="flex justify-between">
                   <div className="text-gray-400">Relocation</div>
-                  <div className="font-medium text-right text-gray-600">No</div>
+                  <div className="font-medium text-right text-gray-600">Yes</div>
                 </div>
               </div>
             </div>
