650deab2dba5d86889390409b0735111b46ffe92
MilanBarande <milan.barande@gmail.com>
2022-09-07 14:53:19 +0200

    :fire: remove Available badge

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -8,9 +8,6 @@ export default function ProfileCard({ getWordingByKey }) {
     <div className="shadow rounded-xl overflow-hidden">
       <div className="h-32 bg-cover cover" />
       <div className="pt-14 p-7 bg-white relative">
-        <span className="status-badge bg-green-400">
-          {getWordingByKey('status')}
-        </span>
         <img src="/avatar.jpeg" alt="Avatar" className="user-photo" />
         <div className="text-lg font-semibold mb-1.5">Milan Barande</div>
         <div className="text-sm text-gray-400 mb-5">
